6fdc31002cc5ef382baebec9c0109c67f11b2467
Devin Frost <devin.frost4311@gmail.com>
2025-07-12 00:21:05 -0400

    Quakaster.h no longer includes all the modules

--- Quakaster/src/Quakaster.h
+++ Quakaster/src/Quakaster.h
@@ -6,25 +6,31 @@
 
 
 
-#define QK_GRAPHICS_BACKEND_GL
+
 #define INCLUDE_QKENT
 
 // Includes all the core functionalities
 // This NEEDS to be loaded first so that proceeding includes have access to it.
 #include "Quakaster/Core.h"
-#include "Quakaster/qklog/logging.h"
 #include "Quakaster/EntryPoint.h"
-
-#include "Quakaster/qkmath/qkmath.h"
 
 // load the appropriate backend
 
-#ifdef QK_GRAPHICS_BACKEND_GL
-#include "Quakaster/qkg/GL/import.h"
-#endif
+namespace qkg
+{
+    enum class Backend {
+        NONE,
+        GL,
+        VK,
+        DX,
+        // Add more as needed
+    };
+}
 
-#include "Quakaster/qkui/import.h"
 
-#ifdef INCLUDE_QKENT
-	//#include "Quakaster/qkent/"
-#endif
+
+
+
+//#include "Quakaster/qkui/import.h"
+
+
