e2eb93689f594b3f95d61857a14803ad5a0d1116
Devin Frost <devin.frost4311@gmail.com>
2025-07-26 17:33:05 -0400

    initial commit

---
+++ Quakaster/src/Quakaster/qkg/GL/MeshInstance.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../../Core.h"
+#include "GL/glew.h"
+
+alignas(32) class QK_API MeshInstance
+{
+	public:
+
+	// Which mesh configuration we are using
+	GLuint m_VAO = 0;
+
+	// which mesh we are using
+	GLuint m_VBO = 0;
+
+	// Which mesh instance we are working with
+	uint32_t m_InstanceIndex = 0;
+
+
+	// Sets an instance_t, this is NOT a complete concept, here for future reference
+
+	//template <class instance_t>
+	//void upload_data(const instance_t& new_value, qkg::MeshRegistry& registry);
+};
