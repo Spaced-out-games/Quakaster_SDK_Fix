0469c788c0206296d9c777676f940aa1ae0e37ca
Devin Frost <devin.frost4311@gmail.com>
2025-08-13 00:50:48 -0400

    Initial commit, nothing functionally added

---
+++ Quakaster/src/Quakaster/qkg/UberShaderGenerator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../Core.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+namespace qk
+{
+
+	// start thinking of some ideas
+	struct QK_API UberShaderFactory
+	{
+	};
+}
