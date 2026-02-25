c22fb906f750e1c14164254229b9bdd1f1594cdd
Devin Frost <devin.frost4311@gmail.com>
2026-02-25 15:16:54 -0500

    Added glfw_EventTranslator

---
+++ Quakaster/core/glfw_EventTranslator.cpp
@@ -0,0 +1,30 @@
+#include "glfw_EventTranslator.h"
+#include "DefaultEvents.h"
+#include "IEvent.h"
+#include "Window.h"
+
+
+namespace qk::EventCallbacks {
+	void on_key(GLFWwindow* window, int key, int scancode, int action, int mods) {
+		Window* target = (Window*)glfwGetWindowUserPointer(window);
+
+
+		if (!target->queue()) return;
+
+		KeyEvent& kv = (KeyEvent&)target->queue()->emplace_back();
+
+		kv.m_Category = EEventCategory::cat_keyboard;
+		switch (action) {
+			case GLFW_PRESS:  kv.m_Type = EEventType::KeyPress_evt; break;
+			case GLFW_REPEAT: kv.m_Type = EEventType::KeyHold_evt;  break;
+			case GLFW_RELEASE:kv.m_Type = EEventType::KeyRelease_evt; break;
+		}
+
+
+
+		kv.key() = key;
+		kv.scancode() = scancode;
+		kv.mods() = mods;
+
+	}
+}
---
+++ Quakaster/core/glfw_EventTranslator.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <GLFW/glfw3.h>
+
+namespace qk::EventCallbacks {
+
+
+
+	void on_key(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+}
+
