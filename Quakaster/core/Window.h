6a7f42ce83fe009d3066402dfde8d8557f3a58ef
Devin Frost <devin.frost4311@gmail.com>
2026-02-25 15:19:37 -0500

    Added a bunch of getters and setters Added pollEvents() (NEEDS DEPRECATED) Window::init() now auto-registers 'this' as a user pointer Window::init() now auto-registers key callback function pointer

--- Quakaster/core/Window.h
+++ Quakaster/core/Window.h
@@ -2,10 +2,14 @@
 #include "../core.h"
 #define GLFW_INCLUDE_NONE
 #include <string>
+#include <memory>
+#include <glm/vec2.hpp>
+#include "EventQueue.h"
 
 
 struct GLFWwindow;
 struct GLFWmonitor;
+typedef void (*GLFWkeyfun)(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 namespace qk {
 
@@ -14,9 +18,24 @@ namespace qk {
 
 	class QK_API Window {
 		GLFWwindow* m_Window = nullptr;
+		std::shared_ptr<EventQueue> m_Queue = nullptr;
+
+		
 		public:
+			void set_key_callback(GLFWkeyfun callback);
+			void set_event_queue(std::shared_ptr<EventQueue> queue);
+			EventQueue* queue();
+			glm::ivec2 get_position();
+			void set_position(glm::ivec2 new_position);
+
+			std::string get_title();
+
 			void init(int initial_width, int initial_height, const std::string& title, Monitor monitor = nullptr, Window* shared = nullptr);
 			Window();
 			~Window();
+
+			
+			void pollEvents();
+
 	};
 }
