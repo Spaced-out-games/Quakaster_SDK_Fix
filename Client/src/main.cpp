06057bd9695d84afebef3954e1f71fba19aef73d
Devin Frost <devin.frost4311@gmail.com>
2026-02-25 19:55:34 -0500

    Added test driver code for layers, layerstacks, and a service manager.

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -4,16 +4,41 @@
 #include "core/EventQueue.h"
 #include <thread>
 #include <stdint.h>
+#include "core/DefaultEvents.h"
+#include "core/LayerStack.h"
+#include "core/ServiceManager.h"
+
+
+#include "DummyService.h"
+#include "TestLayer.h"
+#include "GreetLayer.h"
+
 struct MyApp : qk::Application {
 	qk::Window window;
 	std::shared_ptr<qk::EventQueue> queue;
+	qk::LayerStack stack;
 	size_t max = 0;
+	qk::ServiceManager mgr;
+	entt::registry registry;
 
 	void init(int argc, char** argv) override {
 		qk::init();
 		queue = std::make_unique<qk::EventQueue>();
 		window.init(480, 480, "Demo");
 		window.set_event_queue(queue);
+		stack.attach_queue(queue);
+		stack.insert_layer(std::make_unique<TestLayer>());
+		stack.insert_layer(std::make_unique<GreetLayer>());
+		mgr.registry = &registry;
+		mgr.add_service<DummyService>("DummyService");
+
+		DummyService* dummy = dynamic_cast<DummyService*>(mgr.get_service("DummyService"));
+
+		if (dummy) std::cout << dummy->name;
+
+
+		mgr.remove_service("DummyService");
+
 	}
 
 	void run() override {
@@ -24,6 +49,8 @@ struct MyApp : qk::Application {
 		window.queue()->clear();
 		std::this_thread::sleep_for(std::chrono::milliseconds(17));
 		window.pollEvents();
+		stack.propagate_events();
+
 
 	}
 
