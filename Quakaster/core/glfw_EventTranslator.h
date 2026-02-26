0b8b332dd64faa32e643156f0114083484702f5d
Devin Frost <devin.frost4311@gmail.com>
2026-02-25 19:53:14 -0500

    Added a QK_API declaration I forgot

--- Quakaster/core/glfw_EventTranslator.h
+++ Quakaster/core/glfw_EventTranslator.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <GLFW/glfw3.h>
+#include "../core.h"
 
 namespace qk::EventCallbacks {
 
 
 
-	void on_key(GLFWwindow* window, int key, int scancode, int action, int mods);
+	void QK_API on_key(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 }
 
