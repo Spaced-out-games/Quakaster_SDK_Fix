1d4a84dd71bc91784e9a33e825fbc977da5cf5f4
Devin Frost <devin.frost4311@gmail.com>
2026-03-02 13:00:00 -0500

    Added SystemStack

--- Quakaster/core/ISystem.h
+++ Quakaster/core/ISystem.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <entt/entity/registry.hpp>
+#include <vector>
 
 namespace qk {
 	using ISystem = void(*)(entt::registry&);
+
+	using SystemStack = std::vector<ISystem*>;
+
 }
