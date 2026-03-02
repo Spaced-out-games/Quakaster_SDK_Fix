368a0ca558698c41ad02a731cd0c8724d1d58159
Devin Frost <devin.frost4311@gmail.com>
2026-03-02 02:14:42 -0500

    Added gfx.h/.cpp

---
+++ Quakaster/gfx/gfx.cpp
@@ -0,0 +1,28 @@
+#include "gfx.h"
+#include <stdexcept>
+#include "spdlog/spdlog.h"
+#include "GL/glew.h"
+
+
+namespace gfx {
+	void init() {
+		glewExperimental = GL_TRUE;
+		if (glewInit() != GLEW_OK) {
+			throw std::runtime_error("Failed to initialize GLEW");
+		}
+		spdlog::info("gfx initialized successfully!...");
+	}
+
+	void close() {
+		// There is no GlewTerminate() 
+		// or similiar but for consistency...
+		spdlog::info("gfx shutting down...");
+
+	}
+
+
+	void test() {
+		glClearColor(0.1f, 0.6f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+}
---
+++ Quakaster/gfx/gfx.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "../core.h"
+
+namespace gfx {
+
+
+	// Initializes GLEW  ¯\_(ツ)_/¯
+	void QK_API init();
+
+	// Disables GLEW (lie) (real)
+	void QK_API close();
+
+	// test glClear
+	void QK_API test();
+
+}
