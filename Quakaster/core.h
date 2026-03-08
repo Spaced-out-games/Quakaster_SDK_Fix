2a301c577f692547d65d526bbc9417f25a594349
Devin Frost <devin.frost4311@gmail.com>
2026-03-07 20:03:03 -0500

    Added some macro magic for static linking .Just need to actually set everything up

--- Quakaster/core.h
+++ Quakaster/core.h
@@ -1,14 +1,16 @@
 #pragma once
-
 // Defines the DLL API
 
-#ifdef QK_BUILD_DLL
-	#define QK_API __declspec(dllexport)
+#if defined(QK_STATIC)
+#define QK_API
+#elif defined(QK_BUILD_DLL)
+#define QK_API __declspec(dllexport)
 #else
-	#define QK_API __declspec(dllimport)
+#define QK_API __declspec(dllimport)
 #endif
 
 namespace qk {
+
 	QK_API bool init(int major = 3, int minor = 3);
 	QK_API void close();
 
