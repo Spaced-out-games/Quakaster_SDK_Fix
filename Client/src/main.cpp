79a4037076e7430492180442fa02a00c426703f3
Devin Frost <devin.frost4311@gmail.com>
2026-03-12 00:17:49 -0400

    Added some texture testing Reintegrated queue

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -8,11 +8,12 @@
 #include "core/io/DefaultEvents.h"
 #include "core/io/LayerStack.h"
 #include "core/utility/ServiceManager.h"
+#include "core/res/Image.h"
 #include "services/entt_service_storage.h"
 
 #include "gfx/gfx.h"
 #include "GL/glew.h"
-#include "gui/GUIService.h"
+#include "gui/gui.h"
 
 
 #include "glm/vec2.hpp"
@@ -27,30 +28,41 @@
 #include "gfx/VertexArray.h"
 #include "gfx/ShaderProgram.h"
 #include "gfx/Shader.h"
+#include "gfx/Texture.h"
+#include "DebugEventLayer.h"
+
+struct texPoint2D {
+	glm::vec2 position;
+	glm::vec2 uv;
+};
 
 
-const std::vector<glm::vec2> points = {
-	glm::vec2{0.0, 0.0},
-	glm::vec2{0.0, 1.0},
-	glm::vec2{1.0, 0.0},
-	glm::vec2{1.0, 1.0}
+const std::vector<texPoint2D> points = {
+	{{0.0, 0.0}, {0.0,1.0}},
+	{{0.0, 1.0}, {0.0,0.0}},
+	{{1.0, 0.0}, {1.0,1.0}},
+	{{1.0, 1.0}, {1.0,0.0}}
 };
 
 const char* vertexShaderSrc = R"(
 #version 330 core
 layout(location = 0) in vec2 aPos;
-
+layout(location = 1) in vec2 aTexCoord;
+out vec2 TexCoord;
 void main() {
     gl_Position = vec4(aPos, 0.0, 1.0);
+	TexCoord = aTexCoord;
 }
 )";
 
 const char* fragShaderSrc = R"frag(
 #version 330 core
 out vec4 FragColor;
+in vec2 TexCoord;
+uniform sampler2D texture1;
 
 void main() {
-    FragColor = vec4(0.5);
+    FragColor = texture(texture1, TexCoord);
 }
 )frag";
 #include <vector>
@@ -65,13 +77,13 @@ struct MyApp : qk::Application {
 	std::shared_ptr<qk::EventQueue> queue;
 	qk::LayerStack stack;
 	entt::registry registry;
-	qk::ent::SystemStack systems;
+	std::vector<qk::ent::System> systems;
 
 	gfx::VertexArray arr;
-	gfx::VertexBuffer<glm::vec2> buffer;
+	gfx::VertexBuffer<texPoint2D> buffer;
 	gfx::VertexBufferLayout layout;
 	gfx::ShaderProgram program;
-
+	gfx::Texture texture;
 
 
 
@@ -118,7 +130,11 @@ struct MyApp : qk::Application {
 		}*/
 
 
-		
+		queue = std::make_shared<qk::EventQueue>();
+
+		//auto pLayer = std::make_unique<qk::DebugEventLayer>();
+
+		//stack.insert_layer(std::move(pLayer));
 
 		// Set up the application
 		Application::init(argc, argv);
@@ -131,14 +147,23 @@ struct MyApp : qk::Application {
 		//auto& evt_queue = registry.ctx().emplace<qk::EventQueue>();
 		//stack.attach_queue(&evt_queue);
 
+
+
+
 		// Window initialization
 		window.init(qk::Window::Size{ 480, 480 }, "Demo");
 		window.make_context_current();
+		window.set_event_queue(queue.get());
 
 		// initialize gui and gfx
 		gui::init(window.handle());
 		gfx::init();
 
+		qk::Image img("C:/Users/devin/Desktop/morty.jpg");
+
+		texture.init(img, GL_TEXTURE_2D);
+
+		layout.push<float>(2, false);
 		layout.push<float>(2, false);
 
 
@@ -161,6 +186,8 @@ struct MyApp : qk::Application {
 
 		program.init(hFrag, hVert);
 		program.bind();
+		stack.attach_queue(queue.get());
+
 	}
 
 
@@ -176,6 +203,7 @@ struct MyApp : qk::Application {
 		window.swap_buffers();
 		window.pollEvents();
 		stack.propagate_events();
+		queue->clear();
 		set_status(window.should_close());
 		
 	}
