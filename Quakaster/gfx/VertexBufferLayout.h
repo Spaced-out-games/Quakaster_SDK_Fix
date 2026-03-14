721bfb24ae25318b815bcf08e9f6b95ffa8cbb4b
Devin Frost <devin.frost4311@gmail.com>
2026-03-14 16:12:04 -0400

    Moved get_attribute_size() to the implementation, to prevent double-definitions

--- Quakaster/gfx/VertexBufferLayout.h
+++ Quakaster/gfx/VertexBufferLayout.h
@@ -20,23 +20,7 @@ namespace gfx
     };
 
 
-    unsigned int get_attribute_size(VertexAttribute attribute) {
-        switch (attribute)
-        {
-            case GL_FLOAT:
-                return sizeof(float);
-            case GL_INT:
-                return sizeof(int);
-            case GL_UNSIGNED_BYTE:
-                return sizeof(unsigned char);
-
-        default:
-            __debugbreak();
-            return 0;
-            break;
-        }
-        return 0;
-    }
+    unsigned int get_attribute_size(VertexAttribute attribute);
 
     
 
