b6b5ec35d88b722c8269a8cf3d8ae1b998255970
Devin Frost <devin.frost4311@gmail.com>
2026-03-14 03:14:51 -0400

    ImGui wrapped

--- Quakaster/gui/gui.cpp
+++ Quakaster/gui/gui.cpp
@@ -1,41 +1,367 @@
 #include "gui.h"
-#include "GLFW/glfw3.h"	
+#include "GLFW/glfw3.h"
 
 #include "imgui/imgui.h"
 #include "imgui/backends/imgui_impl_glfw.h"
 #include "imgui/backends/imgui_impl_opengl3.h"
 
 namespace gui {
-	// in a real implementation, just use qk::Window&
-	void init(void* window) {
-		IMGUI_CHECKVERSION();
-		ImGui::CreateContext();
-		ImGuiIO& io = ImGui::GetIO(); (void)io;
-		ImGui::StyleColorsDark();
 
-		ImGui_ImplGlfw_InitForOpenGL((GLFWwindow*)window, false);
-		ImGui_ImplOpenGL3_Init("#version 330");
-	}
-	void demo() {
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+    // In a real implementation, just use qk::Window&
+    void mount(void* window)
+    {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
 
-		// Create a simple ImGui window
-		ImGui::Begin("Hello, ImGui!");
-		ImGui::Text("This is a simple example.");
-		ImGui::End();
+        ImGuiIO& io = ImGui::GetIO();
+        (void)io;
 
-		// Render ImGui
-		ImGui::Render();
+        ImGui::StyleColorsDark();
 
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-	}
+        ImGui_ImplGlfw_InitForOpenGL((GLFWwindow*)window, false);
+        ImGui_ImplOpenGL3_Init("#version 330");
+    }
 
-	void shutdown() {
-		ImGui_ImplOpenGL3_Shutdown();
-		ImGui_ImplGlfw_Shutdown();
-		ImGui::DestroyContext();
-	}
+
+
+
+    void begin_frame()
+    {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+    }
+
+    void end_frame()
+    {
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    }
+
+    bool begin(const char* name, bool* p_open, ImGuiWindowFlags flags)
+    {
+        return ImGui::Begin(name, p_open, flags);
+    }
+
+    void end()
+    {
+        ImGui::End();
+    }
+
+    bool begin_child(const char* str_id, const ImVec2& size, bool border, ImGuiWindowFlags flags)
+    {
+        return ImGui::BeginChild(str_id, size, border, flags);
+    }
+
+    void end_child()
+    {
+        ImGui::EndChild();
+    }
+
+    void dismount()
+    {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+    }
+
+    bool begin_tooltip()
+    {
+        return ImGui::BeginTooltip();
+    }
+
+    void end_tooltip()
+    {
+        ImGui::EndTooltip();
+    }
+
+    void set_tooltip(const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        ImGui::SetTooltipV(fmt, args);
+        va_end(args);
+    }
+
+    void separator() { ImGui::Separator(); }
+    void same_line(float offset_from_start_x, float spacing) { ImGui::SameLine(offset_from_start_x, spacing); }
+    void new_line() { ImGui::NewLine(); }
+    void spacing() { ImGui::Spacing(); }
+    void dummy(const ImVec2& size) { ImGui::Dummy(size); }
+    void indent(float indent_w) { ImGui::Indent(indent_w); }
+    void unindent(float indent_w) { ImGui::Unindent(indent_w); }
+    void begin_group() { ImGui::BeginGroup(); }
+    void end_group() { ImGui::EndGroup(); }
+
+
+
+
+
+
+
+    void push_style_color(ImGuiCol idx, ImU32 col) { ImGui::PushStyleColor(idx, col); }
+    void push_style_color(ImGuiCol idx, const ImVec4& col) { ImGui::PushStyleColor(idx, col); }
+    void pop_style_color(int count) { ImGui::PopStyleColor(count); }
+
+    void push_style_var(ImGuiStyleVar idx, float val) { ImGui::PushStyleVar(idx, val); }
+    void push_style_var(ImGuiStyleVar idx, const ImVec2& val) { ImGui::PushStyleVar(idx, val); }
+    void pop_style_var(int count) { ImGui::PopStyleVar(count); }
+
+    void push_id(const char* str_id) { ImGui::PushID(str_id); }
+    void push_id(const void* ptr_id) { ImGui::PushID(ptr_id); }
+    void push_id(int int_id) { ImGui::PushID(int_id); }
+    void pop_id() { ImGui::PopID(); }
+
+    void text(const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        ImGui::TextV(fmt, args);
+        va_end(args);
+    }
+
+    void text_colored(const ImVec4& col, const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        ImGui::TextColoredV(col, fmt, args);
+        va_end(args);
+    }
+
+    void text_wrapped(const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        ImGui::TextWrappedV(fmt, args);
+        va_end(args);
+    }
+
+    void label_text(const char* label, const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        ImGui::LabelTextV(label, fmt, args);
+        va_end(args);
+    }
+
+    bool button(const char* label, const ImVec2& size)
+    {
+        return ImGui::Button(label, size);
+    }
+
+    bool small_button(const char* label)
+    {
+        return ImGui::SmallButton(label);
+    }
+
+    bool checkbox(const char* label, bool* v)
+    {
+        return ImGui::Checkbox(label, v);
+    }
+
+    bool radio_button(const char* label, bool active)
+    {
+        return ImGui::RadioButton(label, active);
+    }
+
+    bool radio_button(const char* label, int* v, int v_button)
+    {
+        return ImGui::RadioButton(label, v, v_button);
+    }
+
+    bool selectable(const char* label, bool selected, ImGuiSelectableFlags flags, const ImVec2& size)
+    {
+        return ImGui::Selectable(label, selected, flags, size);
+    }
+
+    void progress_bar(float fraction, const ImVec2& size_arg, const char* overlay)
+    {
+        ImGui::ProgressBar(fraction, size_arg, overlay);
+    }
+
+    bool begin_combo(const char* label, const char* preview_value, ImGuiComboFlags flags)
+    {
+        return ImGui::BeginCombo(label, preview_value, flags);
+    }
+
+    void end_combo()
+    {
+        ImGui::EndCombo();
+    }
+
+    bool combo(const char* label, int* current_item, const char* const items[], int items_count, int popup_max_height_in_items)
+    {
+        return ImGui::Combo(label, current_item, items, items_count, popup_max_height_in_items);
+    }
+
+    bool input_text(const char* label, char* buf, size_t buf_size, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* user_data)
+    {
+        return ImGui::InputText(label, buf, buf_size, flags, callback, user_data);
+    }
+
+    bool input_float(const char* label, float* v, float step, float step_fast, const char* format, ImGuiInputTextFlags flags)
+    {
+        return ImGui::InputFloat(label, v, step, step_fast, format, flags);
+    }
+
+    bool input_int(const char* label, int* v, int step, int step_fast, ImGuiInputTextFlags flags)
+    {
+        return ImGui::InputInt(label, v, step, step_fast, flags);
+    }
+
+    bool slider_float(const char* label, float* v, float v_min, float v_max, const char* format, ImGuiSliderFlags flags)
+    {
+        return ImGui::SliderFloat(label, v, v_min, v_max, format, flags);
+    }
+
+    bool slider_int(const char* label, int* v, int v_min, int v_max, const char* format, ImGuiSliderFlags flags)
+    {
+        return ImGui::SliderInt(label, v, v_min, v_max, format, flags);
+    }
+
+    bool drag_float(const char* label, float* v, float v_speed, float v_min, float v_max, const char* format, ImGuiSliderFlags flags)
+    {
+        return ImGui::DragFloat(label, v, v_speed, v_min, v_max, format, flags);
+    }
+
+    bool drag_int(const char* label, int* v, float v_speed, int v_min, int v_max, const char* format, ImGuiSliderFlags flags)
+    {
+        return ImGui::DragInt(label, v, v_speed, v_min, v_max, format, flags);
+    }
+
+    bool tree_node(const char* label)
+    {
+        return ImGui::TreeNode(label);
+    }
+
+    bool tree_node_ex(const char* label, ImGuiTreeNodeFlags flags)
+    {
+        return ImGui::TreeNodeEx(label, flags);
+    }
+
+    void tree_pop()
+    {
+        ImGui::TreePop();
+    }
+
+    bool collapsing_header(const char* label, ImGuiTreeNodeFlags flags)
+    {
+        return ImGui::CollapsingHeader(label, flags);
+    }
+
+    bool begin_popup(const char* str_id, ImGuiWindowFlags flags)
+    {
+        return ImGui::BeginPopup(str_id, flags);
+    }
+
+    bool begin_popup_modal(const char* name, bool* p_open, ImGuiWindowFlags flags)
+    {
+        return ImGui::BeginPopupModal(name, p_open, flags);
+    }
+
+    void end_popup()
+    {
+        ImGui::EndPopup();
+    }
+
+    void open_popup(const char* str_id, ImGuiPopupFlags popup_flags)
+    {
+        ImGui::OpenPopup(str_id, popup_flags);
+    }
+
+    void close_current_popup()
+    {
+        ImGui::CloseCurrentPopup();
+    }
+
+    bool begin_tab_bar(const char* str_id, ImGuiTabBarFlags flags)
+    {
+        return ImGui::BeginTabBar(str_id, flags);
+    }
+
+    void end_tab_bar()
+    {
+        ImGui::EndTabBar();
+    }
+
+    bool begin_tab_item(const char* label, bool* p_open, ImGuiTabItemFlags flags)
+    {
+        return ImGui::BeginTabItem(label, p_open, flags);
+    }
+
+    void end_tab_item()
+    {
+        ImGui::EndTabItem();
+    }
+
+    bool begin_main_menu_bar()
+    {
+        return ImGui::BeginMainMenuBar();
+    }
+
+    void end_main_menu_bar()
+    {
+        ImGui::EndMainMenuBar();
+    }
+
+    bool begin_menu(const char* label, bool enabled)
+    {
+        return ImGui::BeginMenu(label, enabled);
+    }
+
+    void end_menu()
+    {
+        ImGui::EndMenu();
+    }
+
+    bool menu_item(const char* label, const char* shortcut, bool selected, bool enabled)
+    {
+        return ImGui::MenuItem(label, shortcut, selected, enabled);
+    }
+
+    bool begin_table(const char* str_id, int columns, ImGuiTableFlags flags, const ImVec2& outer_size, float inner_width)
+    {
+        return ImGui::BeginTable(str_id, columns, flags, outer_size, inner_width);
+    }
+
+    void end_table()
+    {
+        ImGui::EndTable();
+    }
+
+    void table_next_row(ImGuiTableRowFlags row_flags, float min_row_height)
+    {
+        ImGui::TableNextRow(row_flags, min_row_height);
+    }
+
+    bool table_next_column()
+    {
+        return ImGui::TableNextColumn();
+    }
+
+    void table_setup_column(const char* label, ImGuiTableColumnFlags flags, float init_width_or_weight, ImGuiID user_id)
+    {
+        ImGui::TableSetupColumn(label, flags, init_width_or_weight, user_id);
+    }
+
+    void table_headers_row()
+    {
+        ImGui::TableHeadersRow();
+    }
+
+    ImDrawList* get_window_draw_list()
+    {
+        return ImGui::GetWindowDrawList();
+    }
+
+    ImDrawList* get_background_draw_list()
+    {
+        return ImGui::GetBackgroundDrawList();
+    }
+
+    ImDrawList* get_foreground_draw_list()
+    {
+        return ImGui::GetForegroundDrawList();
+    }
 
 }
--- Quakaster/gui/gui.h
+++ Quakaster/gui/gui.h
@@ -1,14 +1,109 @@
 #pragma once
 #include "../core.h"
+#include "imgui/imgui.h"
+
 
 
 namespace gui {
 
-	// in a real implementation, just use qk::Window&
-	void QK_API init(void* window);
+	void QK_API mount(void* window);
+	void QK_API dismount();
 
-	void QK_API demo();
+	void QK_API separator();
+	void QK_API same_line(float offset_from_start_x = 0.0f, float spacing = -1.0f);
+	void QK_API new_line();
+	void QK_API spacing();
+	void QK_API dummy(const ImVec2& size);
+	void QK_API indent(float indent_w = 0.0f);
+	void QK_API unindent(float indent_w = 0.0f);
+	void QK_API begin_group();
+	void QK_API end_group();
 
-	void QK_API shutdown();
+	void QK_API begin_frame();
+	void QK_API end_frame();
+
+	bool QK_API begin_child(const char* str_id, const ImVec2& size = ImVec2(0, 0), bool border = false, ImGuiWindowFlags flags = 0);
+	void QK_API end_child();
+
+	bool QK_API begin(const char* name, bool* p_open = NULL, ImGuiWindowFlags flags = 0);
+	void QK_API end();
+
+
+	void QK_API push_style_color(ImGuiCol idx, ImU32 col);
+	void QK_API push_style_color(ImGuiCol idx, const ImVec4& col);
+	void QK_API pop_style_color(int count = 1);
+
+	void QK_API push_style_var(ImGuiStyleVar idx, float val);
+	void QK_API push_style_var(ImGuiStyleVar idx, const ImVec2& val);
+	void QK_API pop_style_var(int count = 1);
+
+	void QK_API push_id(const char* str_id);
+	void QK_API push_id(const void* ptr_id);
+	void QK_API push_id(int int_id);
+	void QK_API pop_id();
+
+	void QK_API text(const char* fmt, ...);
+	void QK_API text_colored(const ImVec4& col, const char* fmt, ...);
+	void QK_API text_wrapped(const char* fmt, ...);
+	void QK_API label_text(const char* label, const char* fmt, ...);
+
+	bool QK_API button(const char* label, const ImVec2& size = ImVec2(0, 0));
+	bool QK_API small_button(const char* label);
+	bool QK_API checkbox(const char* label, bool* v);
+	bool QK_API radio_button(const char* label, bool active);
+	bool QK_API radio_button(const char* label, int* v, int v_button);
+	bool QK_API selectable(const char* label, bool selected = false, ImGuiSelectableFlags flags = 0, const ImVec2& size = ImVec2(0, 0));
+	void QK_API progress_bar(float fraction, const ImVec2& size_arg = ImVec2(-FLT_MIN, 0), const char* overlay = NULL);
+
+	bool QK_API begin_combo(const char* label, const char* preview_value, ImGuiComboFlags flags = 0);
+	void QK_API end_combo();
+	bool QK_API combo(const char* label, int* current_item, const char* const items[], int items_count, int popup_max_height_in_items = -1);
+
+	bool QK_API input_text(const char* label, char* buf, size_t buf_size, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = NULL, void* user_data = NULL);
+	bool QK_API input_float(const char* label, float* v, float step = 0.0f, float step_fast = 0.0f, const char* format = "%.3f", ImGuiInputTextFlags flags = 0);
+	bool QK_API input_int(const char* label, int* v, int step = 1, int step_fast = 100, ImGuiInputTextFlags flags = 0);
+
+	bool QK_API slider_float(const char* label, float* v, float v_min, float v_max, const char* format = "%.3f", ImGuiSliderFlags flags = 0);
+	bool QK_API slider_int(const char* label, int* v, int v_min, int v_max, const char* format = "%d", ImGuiSliderFlags flags = 0);
+
+	bool QK_API drag_float(const char* label, float* v, float v_speed = 1.0f, float v_min = 0.0f, float v_max = 0.0f, const char* format = "%.3f", ImGuiSliderFlags flags = 0);
+	bool QK_API drag_int(const char* label, int* v, float v_speed = 1.0f, int v_min = 0, int v_max = 0, const char* format = "%d", ImGuiSliderFlags flags = 0);
+
+	bool QK_API tree_node(const char* label);
+	bool QK_API tree_node_ex(const char* label, ImGuiTreeNodeFlags flags = 0);
+	void QK_API tree_pop();
+	bool QK_API collapsing_header(const char* label, ImGuiTreeNodeFlags flags = 0);
+
+	bool QK_API begin_popup(const char* str_id, ImGuiWindowFlags flags = 0);
+	bool QK_API begin_popup_modal(const char* name, bool* p_open = NULL, ImGuiWindowFlags flags = 0);
+	void QK_API end_popup();
+	void QK_API open_popup(const char* str_id, ImGuiPopupFlags popup_flags = 0);
+	void QK_API close_current_popup();
+
+	bool QK_API begin_tab_bar(const char* str_id, ImGuiTabBarFlags flags = 0);
+	void QK_API end_tab_bar();
+	bool QK_API begin_tab_item(const char* label, bool* p_open = NULL, ImGuiTabItemFlags flags = 0);
+	void QK_API end_tab_item();
+
+	bool QK_API begin_main_menu_bar();
+	void QK_API end_main_menu_bar();
+	bool QK_API begin_menu(const char* label, bool enabled = true);
+	void QK_API end_menu();
+	bool QK_API menu_item(const char* label, const char* shortcut = NULL, bool selected = false, bool enabled = true);
+
+	bool QK_API begin_table(const char* str_id, int columns, ImGuiTableFlags flags = 0, const ImVec2& outer_size = ImVec2(0.0f, 0.0f), float inner_width = 0.0f);
+	void QK_API end_table();
+	void QK_API table_next_row(ImGuiTableRowFlags row_flags = 0, float min_row_height = 0.0f);
+	bool QK_API table_next_column();
+	void QK_API table_setup_column(const char* label, ImGuiTableColumnFlags flags = 0, float init_width_or_weight = 0.0f, ImGuiID user_id = 0);
+	void QK_API table_headers_row();
+
+	bool QK_API begin_tooltip();
+	void QK_API end_tooltip();
+	void QK_API set_tooltip(const char* fmt, ...);
+
+	QK_API ImDrawList* get_window_draw_list();
+	QK_API ImDrawList* get_background_draw_list();
+	QK_API ImDrawList* get_foreground_draw_list();
 
 }
