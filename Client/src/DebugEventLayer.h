dd695832803142a1a142b32fff77901045e1ca34
Devin Frost <devin.frost4311@gmail.com>
2026-03-18 18:18:23 -0400

    Added implementations for on_render on_event() makes use ot std::format for readability

--- Client/src/DebugEventLayer.h
+++ Client/src/DebugEventLayer.h
@@ -1,23 +1,23 @@
 #pragma once
-#include "core/io/ILayer.h"
+#include "core/io/Layer.h"
 #include "core/io/Event.h"
 #include <iostream>
+#include <format>
 
 namespace qk {
-	struct DebugEventLayer : ILayer {
-		EBlock on_event(const Event& evt) override {
-			std::cout << qk::to_string(evt) << '\n';
-			return EBlock::Pass;
-		}
+    struct DebugEventLayer : Layer {
+        EBlock on_event(const Event& evt) override {
+            std::cout << std::format("{}\n", to_string(evt));
+            return EBlock::Pass;
+        }
 
-		virtual void on_render() override {
+        void on_render() override {}
+        void on_attach() override {}
+        void on_detach() override {}
 
-		}
-
-		DebugEventLayer() {
-			using enum EEventCategory;
-			subscribe(cat_all);
-		}
-
-	};
+        DebugEventLayer() {
+            using enum EEventCategory;
+            subscribe(cat_all);
+        }
+    };
 }
